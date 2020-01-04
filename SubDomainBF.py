117d549e90e3c65b90616d2def0213ce5d2116c9
Mohammed898 <51376376+Mohammed898@users.noreply.github.com>
2020-01-04 16:50:50 +0200

    Add files via upload

---
+++ SubDomainBF.py
@@ -0,0 +1,22 @@
+import requests
+#the domain to scan
+domain = input("Insert Domain To Scan:")
+#read all subdomains
+x = input("dictionary Dir:")
+if x == "" :
+    url = f"http://{domain}"
+    r = requests.get(url)
+    print(r)
+else:
+    print("Discovered SubDomains")
+    file = open(x)
+    #read all content
+    content = file.read()
+    #split by new lines
+    subdomains = content.splitlines()
+    for subdomain in subdomains:
+         # construct the url
+         url = f"http://{domain}.{subdomain}"
+         r = requests.get(url)
+         print(url,":", r)
+         
