50ac48d5d7e11bf402e39b4d5b2bdf0d6bffb87a
MohammedHassan <0xmohammedhassan@gmail.com>
2020-06-21 21:55:12 +0200

    Add files via upload

---
+++ snowcracker.py
@@ -0,0 +1,13 @@
+import os
+import sys
+if len(sys.argv) != 2:
+	print("usage : python snowcracker.py wordlist file")
+else:
+	wordlist = open(sys.argv[1], "r")
+	ct = sys.argv[2]
+	for password in wordlist:
+		stripped_password = ''.join(password.split())
+		str = 'stegsnow -C -Q -p \''+stripped_password+'\' '+ct
+		print(str+"\n")
+		os.system(str)
+		print("\n")
